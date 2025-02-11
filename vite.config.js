37497fc93c06247d4ce1096abb4abdd159ef2dd8
lichin <lichin@lichindeMacBook-Pro.local>
2025-02-12 00:57:53 +0800

    update

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,6 @@ import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 // https://vite.dev/config/
 export default defineConfig({
+  base: 'vitejs-vite-d9n7pc2k',
   plugins: [vue()],
 });
